038552d4055942061a9a364349e4afa9792a6f16
Jonathan Collins <jcollins1991@gmail.com>
2014-01-04 20:37:50 -0800

    separate ui and api

---
+++ ui/server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+  key: fs.readFileSync('privatekey.pem'),
+  cert: fs.readFileSync('certificate.pem')
+};
+
+var app = express();
+
+https.createServer(options, app).listen(3000);
+console.log(__dirname);
+app.use("/", express.static(__dirname));
